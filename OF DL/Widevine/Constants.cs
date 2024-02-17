c303a4feb710355b8244db86b04106a0f3f70d17
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-02-18 00:42:00 +0330

    Delete OF DL directory

--- OF DL/Widevine/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WidevineClient.Widevine
-{
-    public class Constants
-    {
-        public static string WORKING_FOLDER { get; set; } = System.IO.Path.GetFullPath(System.IO.Path.Join(System.IO.Directory.GetCurrentDirectory(), "cdm"));
-        public static string DEVICES_FOLDER { get; set; } = System.IO.Path.GetFullPath(System.IO.Path.Join(WORKING_FOLDER, "devices"));
-    }
-}
